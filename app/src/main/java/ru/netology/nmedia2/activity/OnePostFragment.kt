325559bf0c178b322ad6350e661332d7427a0629
MaxiDezorro <maxidezorro@gmail.com>
2025-07-28 18:20:00 +0300

    Fragment 3

--- app/src/main/java/ru/netology/nmedia2/activity/OnePostFragment.kt
+++ app/src/main/java/ru/netology/nmedia2/activity/OnePostFragment.kt
@@ -1,23 +1,31 @@
 package ru.netology.nmedia2.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import ru.netology.nmedia2.R
+import ru.netology.nmedia2.activity.FeedFragment.Companion.textArgs
+import ru.netology.nmedia2.adapter.OnInteractorListener
+import ru.netology.nmedia2.adapter.bind
 import ru.netology.nmedia2.databinding.FragmentSinglePostBinding
+import ru.netology.nmedia2.dto.Post
 import ru.netology.nmedia2.viewmodel.PostViewModel
 
 class OnePostFragment : Fragment() {
     val viewModel: PostViewModel by viewModels(::requireParentFragment)
-     lateinit var binding: FragmentSinglePostBinding
+    lateinit var binding: FragmentSinglePostBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-         binding = FragmentSinglePostBinding.inflate(
+        binding = FragmentSinglePostBinding.inflate(
             inflater,
             container,
             false
@@ -27,34 +35,81 @@ class OnePostFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+val onInteractorListener = object:  OnInteractorListener {
+    override fun onLike(post: Post) {
+        viewModel.likeById(post.id)
+    }
 
+    override fun onShare(post: Post) {
+        // не явный интент
+        val intent = Intent().apply { // создаем интент и настраиваем через apply
+            action = Intent.ACTION_SEND // константа означающая что отправляем данные
+            putExtra(Intent.EXTRA_TEXT, post.content)// кладем в интент
+            // (ключ для передаваемых данных(EXTRA_TEXT) и данные(контент поста))
+            type = "text/plain" //означает что передаем стандартный тип текстового поля
+        }
+        val shareIntent =
+            Intent.createChooser(intent, getString(R.string.chooser_share_post))
+        // создаем интент на показ Choser'a(меню выбора), куда передаем (интент и стринг(сообщение в меню(название))
+
+        startActivity(shareIntent) // метод активити куда передаем наш интент
+
+        viewModel.shareById(post.id)
+    }
+
+    override fun onRemove(post: Post) {
+        viewModel.removeById(post.id)
+        findNavController().navigateUp()
+    }
+
+    override fun onEdit(post: Post) {
+        viewModel.edit(post)
+        val text = post.content
+        findNavController().navigate(
+            R.id.action_onePostFragment_to_newPostFragment,
+            Bundle().apply {
+                textArgs = text
+            })
+
+    }
+
+    override fun onPlayVideoIntent(post: Post) {
+        val intent = Intent()  // Создаем новый не явный Intent
+        intent.action = Intent.ACTION_VIEW // Указываем действие "просмотр"
+        intent.data = post.videoURL?.toUri() // Преобразуем URL видео в Uri
+        startActivity(intent)
+    }
+
+    override fun onOnePostOpen(post: Post) { }
+
+}
 
         val postId = arguments?.getInt("postId") ?: 0
 
         viewModel.data.observe(viewLifecycleOwner) { posts ->
             val post = posts.find { it.id == postId } ?: return@observe
-            with(binding.postOne) {
-                author.text = post.author
-                content.text = post.content
-                published.text = post.published
-
-                viewsAmount.text = showHowManyIntToString(post.countViews)
-
-                like.apply {
-                    isChecked = post.likeByMe
-                    text = showHowManyIntToString(post.countLikes)
-                }
-
-
-                share.text = showHowManyIntToString(post.countShare)
-
-                if (post.videoURL != null) {
-                    videoLayout.visibility = View.VISIBLE
-                } else {
-                    videoLayout.visibility = View.GONE
-                }
-
-            }
+            binding.postOne.bind(post, onInteractorListener)
+//            with(binding.postOne) {
+//                author.text = post.author
+//                content.text = post.content
+//                published.text = post.published
+//
+//                viewsAmount.text = showHowManyIntToString(post.countViews)
+//
+//                like.apply {
+//                    isChecked = post.likeByMe
+//                    text = showHowManyIntToString(post.countLikes)
+//                }
+//
+//
+//                share.text = showHowManyIntToString(post.countShare)
+//
+//                if (post.videoURL != null) {
+//                    videoLayout.visibility = View.VISIBLE
+//                } else {
+//                    videoLayout.visibility = View.GONE
+//                }
+//            }
         }
     }
 
--- app/src/main/java/ru/netology/nmedia2/adapter/PostAdapter.kt
+++ app/src/main/java/ru/netology/nmedia2/adapter/PostAdapter.kt
@@ -58,71 +58,76 @@ class PostViewHolder(
 
 
 ) : RecyclerView.ViewHolder(binding.root) { // в конструктор вьюхолдера передаем корневую вью карточки
-    fun bind(post: Post) =  // заполняем карточку данными поста
-        with(binding) { // обращаемся ко всем полям binding (вместо binding.author.text итд )
-            author.text = post.author
-            content.text = post.content
-            published.text = post.published
+    fun bind(post: Post) {
+        binding.bind(post, onInteractorListener)
+    }
+//    fun bind(post: Post) =  // заполняем карточку данными поста
+//        with(binding) { // обращаемся ко всем полям binding (вместо binding.author.text итд )
+//            author.text = post.author
+//            content.text = post.content
+//            published.text = post.published
+//
+//            viewsAmount.text = showHowManyIntToString(post.countViews)
+//
+//            like.apply {
+//                isChecked = post.likeByMe
+//                text = showHowManyIntToString(post.countLikes)
+//            }
+//            // без apply так         \\  или с  with(like) {
+////            like.isChecked =         \\  isChecked =
+////            like.text =               \\  text =  }
+//
+//            share.text = showHowManyIntToString(post.countShare)
+//
+//            if (post.videoURL != null) {
+//                videoLayout.visibility = View.VISIBLE
+//            } else {
+//                videoLayout.visibility = View.GONE
+//            }
+//
+//            videoLayout.setOnClickListener {
+//                onInteractorListener.onPlayVideoIntent(post)
+//            }
+//
+//
+//            like.setOnClickListener { // дергаем лямбду
+//                onInteractorListener.onLike(post)
+//            }
+//            share.setOnClickListener {
+//                onInteractorListener.onShare(post)
+//            }
+//            menu.setOnClickListener {
+//                PopupMenu(
+//                    it.context,
+//                    it
+//                ).apply { // передаем в popup(контекст вью, и место появления с нашей вью)
+//
+//                    inflate(R.menu.post_menu) // инфлейтим наше меню
+//                    setOnMenuItemClickListener { item -> // item из post_menu.xml
+//                        when (item.itemId) {
+//                            R.id.remove -> {
+//                                onInteractorListener.onRemove(post)
+//                                true
+//                            }
+//
+//                            R.id.edit -> {
+//                                onInteractorListener.onEdit(post)
+//                                true
+//                            }
+//
+//                            else -> false
+//                        }
+//                    }
+//                }.show()
+//            }
+//
+//            content.setOnClickListener {
+//                onInteractorListener.onOnePostOpen(post)
+//            }
+//
+//        }
 
-            viewsAmount.text = showHowManyIntToString(post.countViews)
 
-            like.apply {
-                isChecked = post.likeByMe
-                text = showHowManyIntToString(post.countLikes)
-            }
-            // без apply так         \\  или с  with(like) {
-//            like.isChecked =         \\  isChecked =
-//            like.text =               \\  text =  }
-
-            share.text = showHowManyIntToString(post.countShare)
-
-            if (post.videoURL != null) {
-                videoLayout.visibility = View.VISIBLE
-            } else {
-                videoLayout.visibility = View.GONE
-            }
-
-            videoLayout.setOnClickListener {
-                onInteractorListener.onPlayVideoIntent(post)
-            }
-
-
-            like.setOnClickListener { // дергаем лямбду
-                onInteractorListener.onLike(post)
-            }
-            share.setOnClickListener {
-                onInteractorListener.onShare(post)
-            }
-            menu.setOnClickListener {
-                PopupMenu(
-                    it.context,
-                    it
-                ).apply { // передаем в popup(контекст вью, и место появления с нашей вью)
-
-                    inflate(R.menu.post_menu) // инфлейтим наше меню
-                    setOnMenuItemClickListener { item -> // item из post_menu.xml
-                        when (item.itemId) {
-                            R.id.remove -> {
-                                onInteractorListener.onRemove(post)
-                                true
-                            }
-
-                            R.id.edit -> {
-                                onInteractorListener.onEdit(post)
-                                true
-                            }
-
-                            else -> false
-                        }
-                    }
-                }.show()
-            }
-
-            content.setOnClickListener {
-                onInteractorListener.onOnePostOpen(post)
-            }
-
-        }
 }
 
 object PostDiffCallBack : DiffUtil.ItemCallback<Post>() {   //  отвечает за сравнение списков
@@ -133,5 +138,63 @@ object PostDiffCallBack : DiffUtil.ItemCallback<Post>() {   //  отвечает
 
     override fun areContentsTheSame(oldItem: Post, newItem: Post): Boolean {
         return oldItem == newItem // сравниваем содержимое постов, не изменились ли данные
+    }
+}
+
+fun CardPostBinding.bind(post: Post, onInteractorListener: OnInteractorListener) {
+    author.text = post.author
+    content.text = post.content
+    published.text = post.published
+
+    viewsAmount.text = showHowManyIntToString(post.countViews)
+
+    like.apply {
+        isChecked = post.likeByMe
+        text = showHowManyIntToString(post.countLikes)
+    }
+
+    share.text = showHowManyIntToString(post.countShare)
+
+    if (post.videoURL != null) {
+        videoLayout.visibility = View.VISIBLE
+    } else {
+        videoLayout.visibility = View.GONE
+    }
+
+    videoLayout.setOnClickListener {
+        onInteractorListener.onPlayVideoIntent(post)
+    }
+
+    like.setOnClickListener {
+        onInteractorListener.onLike(post)
+    }
+
+    share.setOnClickListener {
+        onInteractorListener.onShare(post)
+    }
+
+    menu.setOnClickListener {
+        PopupMenu(it.context, it).apply {
+            inflate(R.menu.post_menu)
+            setOnMenuItemClickListener { item ->
+                when (item.itemId) {
+                    R.id.remove -> {
+                        onInteractorListener.onRemove(post)
+                        true
+                    }
+
+                    R.id.edit -> {
+                        onInteractorListener.onEdit(post)
+                        true
+                    }
+
+                    else -> false
+                }
+            }
+        }.show()
+    }
+
+    content.setOnClickListener {
+        onInteractorListener.onOnePostOpen(post)
     }
 }
